1a3e3748125dfd55e813828fb75e34101047d158
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-02 00:09:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let makeUpperCase = (str) => str.split('').map(letter => letter.toUpperCase()).join('') 
+
+
+/*
+8kyu
+Write a function which converts the input string to uppercase.
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
